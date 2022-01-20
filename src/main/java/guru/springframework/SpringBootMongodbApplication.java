203053367d3181245d221178a228ae661ae98786
Connor Wheatley <cwheatley@tier2consulting.com>
2022-01-20 12:57:42 +0000

    Using docker, was able to have data stored and persist when spring boot was resstarted

--- src/main/java/guru/springframework/SpringBootMongodbApplication.java
+++ src/main/java/guru/springframework/SpringBootMongodbApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootMongodbApplication {
 
+	// main
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootMongodbApplication.class, args);
 	}
